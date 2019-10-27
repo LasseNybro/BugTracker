efd241dad416c96ce93433669a5735a490037f7c
Lasse Nybro Nielsen <lasso290391@hotmail.com>
2019-10-27 22:34:29 +0100

    Included the first basics of a User Object and some work on the UserAPI. Also added an Agile Sprint Plan.txt as a timetable to set goals for this project.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,18 +12,37 @@ namespace BugTracker.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
+            //Get the information from the Database
+
+            //Convert the information into a User Object
+
+            //return a JSON conversion of the User Object
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public JSON Get(int id)
         {
+            //Get the information from the Database from the User id?
+
+            //Convert the information into a User Object
+
+            //return a JSON conversion of the User Object
+
             return "value";
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            //Get the information from the Database from the User id?
+
+            //Compare SHA256 encrypted passwords, include SALT string in the testee, continue if passwords match otherwise break it off here.
+
+            //Convert the information into a User Object
+
+            //return a JSON conversion of the User Object
+
             // Some way of logging into this program, include security later
         }
 
@@ -35,17 +54,6 @@ namespace BugTracker.Controllers
         // DELETE api/values/5
         public void Delete(int id)
         {
-        }
-    }
-    class User
-    {
-        int ID = 0; //Must be unique, since it is used to find reported problems with and also messages.
-        string Level = "User";
-        string Name = ""; //Must be unique, since it is used to login with.
-        string Password = ""; //Has to have some minimum requirements as a password.
-        User(int id, string level, string name, string password)
-        {
-            ID = id; Level = level;Name = name;Password = password;
         }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BugTracker.Models
+{
+    public class User
+    {
+        public int ID { get; set; } //Must be unique, since it is used to find reported problems with and also messages.
+        public string Level { get; set; }
+        public string Name { get; set; } //Must be unique, since it is used to login with. //Should I remove the ID, since I also use this to identify the Users with.
+        public string Password { get; set; } //Has to have some minimum requirements as a password.
+        User(int id, string level, string name, string password)
+        {
+            ID = id; Level = level; Name = name; string _Password = password;
+            Password = sha256_hash(_Password+"E1Y8U7H6GGG43H32H46J21K");
+        }
+        public static String sha256_hash(String value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (Byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+    }
+}
